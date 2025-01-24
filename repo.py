bc937774d0f854523a8d7843c53650ce756a4981
Maria-Gota <85016608+Maria-Gota@users.noreply.github.com>
2025-01-24 19:24:13 +0200

    -- file cleanup

--- repo.py
+++ repo.py
@@ -1,8 +1,9 @@
 import sqlite3
 import hashlib
 import db_constants
-import aes
+import utils
 import generator
+
 
 def exists_vault():
     with sqlite3.connect("../pw_mng.db") as connection:
@@ -12,6 +13,7 @@ def exists_vault():
 
         return count != 0
 
+
 def get_vault_name():
     with sqlite3.connect("../pw_mng.db") as connection:
         cursor = connection.cursor()
@@ -20,11 +22,12 @@ def get_vault_name():
 
     return vault_name
 
+
 def create_vault(vault_name):
     password = generator.generate_bbs_password(
         "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*-_?")
 
-    hashed_pw = aes.hash_password(password)
+    hashed_pw = utils.hash_password(password)
     salt = generator.generate_bbs_salt()
 
     with sqlite3.connect("../pw_mng.db") as connection:
@@ -56,10 +59,10 @@ def retrieve_all_passwords(mpw, vault):
 
     salt = retrieve_e_k_s(vault)
     decrypted_passwords = []
-    decryption_key = aes.derive_encryption_key(mpw, salt)
+    decryption_key = utils.derive_encryption_key(mpw, salt)
 
     for pwd in passwords:
-        decr = aes.decrypt_aes(pwd[3], decryption_key)
+        decr = utils.decrypt_aes(pwd[3], decryption_key)
         new_pwd = (pwd[0], pwd[1], pwd[2], decr)
         decrypted_passwords.append(new_pwd)
 
@@ -67,10 +70,9 @@ def retrieve_all_passwords(mpw, vault):
 
 
 def create_new_password(vault, mpw, what_for, password, uname=None, em_addr=None):
-
     salt = retrieve_e_k_s(vault)
-    encryption_key = aes.derive_encryption_key(mpw, salt)
-    encrypted_pw = aes.encrypt_aes(password, encryption_key)
+    encryption_key = utils.derive_encryption_key(mpw, salt)
+    encrypted_pw = utils.encrypt_aes(password, encryption_key)
 
     with sqlite3.connect("../pw_mng.db") as connection:
         cursor = connection.cursor()
@@ -85,10 +87,12 @@ def retrieve_e_k_s(vault_name):
 
     return e_k_s
 
+
 def delete_password(what_for):
     with sqlite3.connect("../pw_mng.db") as connection:
         cursor = connection.cursor()
         cursor.execute(db_constants.DELETE_PASSWORD, (what_for,))
+
 
 def create_tables():
     with sqlite3.connect("../pw_mng.db") as connection:
--- utils.py
+++ utils.py
@@ -5,63 +5,49 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import hashlib
 
 
+def pad(text):
+    padder = padding.PKCS7(256).padder()
+    padded_text = padder.update(text) + padder.finalize()
+    return padded_text
+
+
+def unpad(padded_plaintext):
+    padding_length = padded_plaintext[-1]
+    return padded_plaintext[:-padding_length]
+
+
 def hash_password(password):
     password = password.encode()
     return hashlib.sha256(password).hexdigest()
 
 
 def derive_encryption_key(password, salt):
-    """
-    Derives the private key from the password using PBKDF2 with HMAC-SHA256.
-    """
-
     password_bytes = password.encode()
-
     kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),  # Use SHA-256 as the underlying hash function
-        length=32,  # Length of the derived key (32 bytes = 256 bits for AES-256)
+        algorithm=hashes.SHA256(),
+        length=32,
         salt=salt,
         iterations=10
     )
-
-    # Derive the key
     derived_key = kdf.derive(password_bytes)
 
     return derived_key
 
 
-# AES encryption function
 def encrypt_aes(plaintext, key):
-    # Convert plaintext to bytes
     plaintext = plaintext.encode()
-
-    # Add PKCS7 padding
-    padder = padding.PKCS7(256).padder()
-    padded_plaintext = padder.update(plaintext) + padder.finalize()
-
-    # Create AES cipher in ECB mode
+    plaintext = pad(plaintext)
     cipher = Cipher(algorithms.AES(key), modes.ECB())
     encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(plaintext) + encryptor.finalize()
 
-    # Encrypt the plaintext
-    ciphertext = encryptor.update(padded_plaintext) + encryptor.finalize()
     return ciphertext
 
 
-# AES decryption function
 def decrypt_aes(ciphertext, key):
-    # Create AES cipher in ECB mode
     cipher = Cipher(algorithms.AES(key), modes.ECB())
     decryptor = cipher.decryptor()
-
-    # Decrypt the ciphertext
     padded_plaintext = decryptor.update(ciphertext) + decryptor.finalize()
-
-    # Remove PKCS7 padding
     unpadded_plaintext = unpad(padded_plaintext)
+
     return unpadded_plaintext.decode()
-
-
-def unpad(padded_plaintext):
-    padding_length = padded_plaintext[-1]  # Get the last byte value
-    return padded_plaintext[:-padding_length]
